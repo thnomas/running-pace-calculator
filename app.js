f9901c235967e1e9bce5390cec1aab26ef641e4c
thnomas <thombot43@gmail.com>
2024-06-13 22:25:59 +0100

    updated port

--- app.js
+++ app.js
@@ -2,14 +2,10 @@
 const express = require("express")
 const app = express()
 
+port = process.env.PORT || 3000
 // use the express-static middleware
 app.use(express.static("public"))
 
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
-
 // start the server listening for requests
-app.listen(process.env.PORT || 3000,
-	() => console.log("Server is running..."));
+app.listen(port,
+	() => console.log(`Server is running on port ${port}.`));
